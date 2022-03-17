72060540d1a80a2fd1205ef5cd4068fd2b4435d4
MaskDuck <it-is@too-obvious-that.maskduck.is-a.dev>
2022-03-17 19:28:31 +0700

    uh updation

--- command_registerer.py
+++ command_registerer.py
@@ -1,10 +1,13 @@
+"""
+This file is used for registering commands.
+"""
 import requests
 
 from os import environ
 
-appid = environ['client_id']
-token = environ['token']
-guild = environ['guild_id']
+appid = environ["client_id"]
+token = environ["token"]
+guild = environ["guild_id"]
 
 url = f"https://discord.com/api/v8/applications/{appid}/guilds/{guild}/commands"
 
@@ -14,7 +17,7 @@ headers = {"Authorization": f"Bot {token}"}
 json = {
     "name": "tag",
     "type": 1,
-    "description": "new tag",
+    "description": "[MAINTAINER ONLY] new tag, or edit a existing tag.",
 }
 
 r = requests.post(url, headers=headers, json=json)
--- main.py
+++ main.py
@@ -1,25 +1,38 @@
-# from pymongo import MongoClient
+from pymongo import MongoClient
 
 from flask import Flask, jsonify, request, Response
 
 from nacl.signing import VerifyKey
 from nacl.exceptions import BadSignatureError
 
+from typing import Final
 from os import environ
+import requests
 
 debug = False
 if debug:
     from dotenv import load_dotenv
+
     load_dotenv()
 
-app= Flask(__name__)
-public_key: str = environ['public_key']
+app = Flask(__name__)
+public_key: str = environ["public_key"]
 verify_key: VerifyKey = VerifyKey(bytes.fromhex(public_key))
+appid = environ["client_id"]
+token = environ["token"]
+guild = environ["guild_id"]
 
+url: Final[str] = f"https://discord.com/api/v8/applications/{appid}/guilds/{guild}/commands"
+mongo: MongoClient = MongoClient(environ["mongo"])["dev"]["storer"]
 
-# mongo: MongoClient = MongoClient(environ['mongo'])["dev"]["storer"]
+maintainers: Final[list[int]] = [
+    716134528409665586,  # MaskDuck
+    911987862981972058,  # Hackermon
+    763767239018938368,  # molai
+    730358289849778186,  # phenax
+    488802888928329753,  # Botly
+]
 
-maintainers: list[int] = [716134528409665586, 911987862981972058, 763767239018938368, 730358289849778186, 488802888928329753]
 
 @app.route("/", methods=["POST"])
 async def handler():
@@ -29,7 +42,7 @@ async def handler():
         signature = request.headers["X-Signature-Ed25519"]
         timestamp = request.headers["X-Signature-Timestamp"]
         body = request.data.decode("utf-8")
-        verify_key.verify(f'{timestamp}{body}'.encode(), bytes.fromhex(signature))
+        verify_key.verify(f"{timestamp}{body}".encode(), bytes.fromhex(signature))
         verified: bool = True
     except BadSignatureError:
         verified: bool = False
@@ -39,39 +52,86 @@ async def handler():
         if data["type"] == 1:
             return jsonify({"type": 1})
         elif data["type"] == 2:
-            if data['data']['name'] == "tag":
-                if int(data['member']['user']['id']) in maintainers:
+            if data["data"]["name"] == "tag":
+                if int(data["member"]["user"]["id"]) in maintainers:
                     modal = {
                         "title": "New Tag",
                         "custom_id": "TAG_Modal",
-                        "components": [{
-                            "type": 1,
-                            "components": [{
-                                "type": 4,
-                                "custom_id": "TAG_Modal_Name",
-                                "style": 1,
-                                "label": "Tag Name",
-                                "max_length": 32
-                            }]
-                        },{
-                            "type": 1,
-                            "components": [{
-                                "type": 4,
-                                "custom_id": "TAG_Modal_Content",
-                                "style": 1,
-                                "label": "Tag Content",
-                                "max_length": 1999
-                            }]
-                        }]
+                        "components": [
+                            {
+                                "type": 1,
+                                "components": [
+                                    {
+                                        "type": 4,
+                                        "custom_id": "TAG_Modal_Name",
+                                        "style": 1,
+                                        "label": "Tag Name",
+                                        "max_length": 32,
+                                        "placeholder": "If you are editing a tag please give the tag name here."
+                                    }
+                                ],
+                            },
+                            {
+                                "type": 1,
+                                "components": [
+                                    {
+                                        "type": 4,
+                                        "custom_id": "TAG_Modal_Content",
+                                        "style": 1,
+                                        "label": "Tag Content",
+                                        "max_length": 1999,
+                                        "placeholder": "Leave this part as blank to delete this tag.",
+                                        "required": False
+                                    }
+                                ],
+                            },
+                        ],
                     }
                     return jsonify({"type": 9, "data": modal})
                 else:
-                    return jsonify({"type": 4, "data": {"content": "You do not have permission to use this command.", "flags": 1 << 6}})
-            elif data['data']['name'] == "ping":
+                    return jsonify(
+                        {
+                            "type": 4,
+                            "data": {
+                                "content": "You do not have permission to use this command.",
+                                "flags": 1 << 6,
+                            },
+                        }
+                    )
+            elif data["data"]["name"] == "ping":
                 return jsonify({"type": 4, "data": {"content": "pong"}})
+            else:
+                existing_data = mongo.find_one({"_id": data["data"]["name"]})
+                return jsonify({"type": 4, "data": {"content": existing_data['content']}})
         elif data["type"] == 5:
-            if data['data']['custom_id'] == "TAG_Modal":
-                return jsonify({"type": 4, "data": {"content": f"tag_name: {data['data']['components'][0]['components'][0]['value']}, tag_value: {data['data']['components'][1]['components'][0]['value']}"}})
+            if data["data"]["custom_id"] == "TAG_Modal": 
+                existing_data = mongo.find_one({"_id": data['data']['components'][0]['components'][0]['value']})
+                editing = False
+                if existing_data:
+                    editing = True
+                if editing:
+                    mongo.update_one({"_id": data['data']['components'][0]['components'][0]['value']}, {"$set": {
+                        "content": data['data']['components'][1]['components'][0]['value']
+                    }})
+                    return jsonify({"type": 4, "data": {"content": "Tag updated."}})
+                else:
+                    insert_data = {"_id": data['data']['components'][0]['components'][0]['value'], "content": data['data']['components'][1]['components'][0]['value']}
+                    mongo.insert_one(insert_data)
+                    json = {
+                        "name": data['data']['components'][0]['components'][0]['value'],
+                        "type": 1,
+                        "description": "custom tag",
+                    }
+                    headers = {"Authorization": f"Bot {token}"}
+                    requests.post(url, headers=headers, json=json)
+                    return jsonify(
+                        {
+                            "type": 4,
+                            "data": {
+                                "content": f"Created a tag with name {data['data']['components'][0]['components'][0]['value']} and content {data['data']['components'][1]['components'][0]['value']}. To edit the tag please contact MaskDuck."
+                            },
+                        }
+                 )
 
 
 if __name__ == "__main__":
